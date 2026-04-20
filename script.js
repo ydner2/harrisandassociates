8c1dcc39654d648ac33e97b2ca3d247009d6d380
Ryden Richards <rsrichards@formerstudents.ucdavis.edu>
2026-04-20 16:22:29 -0700

    finished business section

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
 (function(){
     "use strict";
     console.log("running JS");
+
+
+
+
+
+
+
+
+
+
+
+
+
+//for cards
+document.querySelectorAll('.policy-header').forEach(button => {
+    button.addEventListener('click', () => {
+        const card = button.closest('.policy-card');
+
+        document.querySelectorAll('.policy-card').forEach(item => {
+            item.classList.remove('open');
+            item.querySelector('.policy-header').setAttribute('aria-expanded', 'false');
+        });
+
+        card.classList.add('open');
+        button.setAttribute('aria-expanded', 'true');
+    });
+});
 })();
